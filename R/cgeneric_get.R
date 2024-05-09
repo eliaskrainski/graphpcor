45f69c03768a728d1112d1a41713420aa642e506
Elias T Krainski <eliaskrainski@gmail.com>
2024-05-09 14:25:59 +0300

    add cgeneric_get.R

---
+++ R/cgeneric_get.R
@@ -0,0 +1,68 @@
+#' Function to extract cgeneric model
+#' @param cgeneric_model an object containing the cgeneric model
+#' @param theta a numeric vector with theta.
+#' If NULL (default) will use the initial from the cgeneric model.
+#' @param cmd an string to specify which model element to get
+#' @export
+cgeneric_get <- function(cgeneric_model,
+                         theta = NULL,
+                         cmd = c("graph", "Q", "initial", "mu", "log_prior")) {
+
+  cgdata <- cgeneric_model$f$cgeneric$data
+  stopifnot(!is.null(cgdata))
+  stopifnot(!is.null(cgdata$ints))
+  stopifnot(!is.null(cgdata$doubles))
+  stopifnot(!is.null(cgdata$characters))
+
+  cmds <- c("graph", "Q", "initial", "mu", "log_prior")
+  cmd <- match.arg(cmd,
+                   cmds,
+                   several.ok = TRUE)
+  stopifnot(length(cmd)>0)
+
+  if(is.null(theta)) {
+    theta <- .Call(
+      "cgeneric_element_get",
+      "initial",
+      NULL,
+      cgdata$ints,
+      cgdata$doubles,
+      cgdata$characters,
+      PACKAGE = "corGraphs"
+    )
+  }
+
+  if((length(cmd) == 1)) {
+    if (cmd == "initial") {
+      return(theta)
+    } else {
+      return(
+        .Call(
+          "cgeneric_element_get",
+          cmd,
+          theta,
+          cgdata$ints,
+          cgdata$doubles,
+          cgdata$characters,
+          PACKAGE = "corGraphs"
+        )
+      )
+    }
+  } else {
+    names(cmd) <- cmd
+    return(
+      lapply(cmd, function(x) {
+        .Call(
+          "cgeneric_element_get",
+          x,
+          theta,
+          cgdata$ints,
+          cgdata$doubles,
+          cgdata$characters,
+          PACKAGE = "corGraphs"
+        )
+      })
+    )
+  }
+
+}
