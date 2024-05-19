f76cfb3f0048f6555d4c721a5a1cb1476eba600f
Elias T Krainski <eliaskrainski@gmail.com>
2024-05-19 17:02:15 +0300

    add optimize to cgeneric_get

--- R/cgeneric_get.R
+++ R/cgeneric_get.R
@@ -1,14 +1,18 @@
 #' Function to extract cgeneric model
 #' @param cgeneric_model an object containing the cgeneric model
 #' @param theta a numeric vector with theta.
+#' @param optimize logical indicating if the graph and Q are
+#' returned only the elements (if TRUE) or to be built (if FALSE).
 #' If NULL (default) will use the initial from the cgeneric model.
 #' @param cmd an string to specify which model element to get
 #' @export
 cgeneric_get <- function(cgeneric_model,
                          cmd = c("graph", "Q", "initial", "mu", "log.prior"),
-                         theta = NULL
+                         theta = NULL,
+                         optimize = TRUE
                          ) {
 
+  ret <- NULL
   cmd[cmd == "log.prior"] <- "log_prior"
   cmd <- unique(cmd)
 
@@ -35,31 +39,59 @@ cgeneric_get <- function(cgeneric_model,
     PACKAGE = "corGraphs"
   )
 
-  if(length(cmd) == 1){
+  if(length(cmd) == 1) {
     if(cmd == "initial") {
       return(itheta)
+    }
+    ret <- .Call(
+      "cgeneric_element_get",
+      cmd,
+      theta,
+      cgdata$ints,
+      cgdata$doubles,
+      cgdata$characters,
+      cgdata$matrices,
+      cgdata$smatrices,
+      PACKAGE = "corGraphs"
+      )
+    if(optimize | any(cmd == c("mu", "log_prior"))) {
+      return(ret)
+    }
+    if(cmd != "graph") {
+      ij <- .Call(
+        "cgeneric_element_get",
+        "graph",
+        theta,
+        cgdata$ints,
+        cgdata$doubles,
+        cgdata$characters,
+        cgdata$matrices,
+        cgdata$smatrices,
+        PACKAGE = "corGraphs"
+      )
     } else {
-      return(
-        .Call(
-          "cgeneric_element_get",
-          cmd,
-          theta,
-          cgdata$ints,
-          cgdata$doubles,
-          cgdata$characters,
-          cgdata$matrices,
-          cgdata$smatrices,
-          PACKAGE = "corGraphs"
-          )
-        )
-      }
-    } else {
-    names(cmd) <- cmd
-    if(is.null(theta)) {
-      theta = itheta
+      ij <- ret
+      ret <- rep(1, length(ij[[1]]))
     }
     return(
-      lapply(cmd, function(x) {
+      INLA::inla.as.sparse(
+        sparseMatrix(
+          i = ij[[1]] + 1,
+          j = ij[[2]] + 1,
+          x = ret,
+          symmetric = TRUE
+        )
+      )
+    )
+  }
+
+  names(cmd) <- cmd
+  if(is.null(theta)) {
+    theta = itheta
+  }
+  ret <-
+    lapply(
+      cmd, function(x) {
         .Call(
           "cgeneric_element_get",
           x,
@@ -71,8 +103,46 @@ cgeneric_get <- function(cgeneric_model,
           cgdata$smatrices,
           PACKAGE = "corGraphs"
         )
-      })
+      }
+    )
+  if(optimize) {
+     return(ret)
+  }
+
+  if(any(cmd == "Q")) {
+    if(any(cmd == "graph")) {
+      ij <- ret$graph
+    } else {
+      ij <- .Call(
+        "cgeneric_element_get",
+        "graph",
+        theta,
+        cgdata$ints,
+        cgdata$doubles,
+        cgdata$characters,
+        cgdata$matrices,
+        cgdata$smatrices,
+        PACKAGE = "corGraphs"
+      )
+    }
+    ret$Q <- INLA::inla.as.sparse(
+      sparseMatrix(
+        i = ij[[1]] + 1L,
+        i = ij[[2]] + 1L,
+        x = ret$Q,
+        symmetric = TRUE
+      )
     )
   }
+  if(any(cmd == "graph")) {
+    ret$graph <- INLA::inla.as.sparse(
+      sparseMatrix(
+        i = ret$graph[[1]] + 1L,
+        i = ret$graph[[2]] + 1L,
+        x = 1
+      )
+    )
+  }
+  return(ret)
 
 }
