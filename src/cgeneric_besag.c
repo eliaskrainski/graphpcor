9e762086532aefb179f4209bb04672fb7ffd43ee
Elias T Krainski <eliaskrainski@gmail.com>
2024-06-05 11:10:53 +0300

    add cgeneirc_besag and test

---
+++ src/cgeneric_besag.c
@@ -0,0 +1,152 @@
+
+/* cgeneric_besag.c
+ *
+ * Copyright (C) 2023 Elias Krainski
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * The author's contact information:
+ *
+ *        Elias Krainski
+ *        CEMSE Division
+ *        King Abdullah University of Science and Technology
+ *        Thuwal 23955-6900, Saudi Arabia
+ */
+
+#include "cgeneric_defs.h"
+
+double *inla_cgeneric_besag(inla_cgeneric_cmd_tp cmd, double *theta, inla_cgeneric_data_tp * data) {
+
+  //  Q = \tau*R
+
+  double *ret = NULL, prec;
+
+  if (theta) {
+    prec = exp(theta[0]);
+  } else {
+    prec = NAN;
+  }
+
+  // the size of the model
+  assert(data->n_ints > 1);
+  assert(!strcasecmp(data->ints[0]->name, "n"));	       // this will always be the case
+  int N = data->ints[0]->ints[0];			       // this will always be the case
+  assert(N > 0);
+
+  assert(!strcasecmp(data->ints[1]->name, "debug"));	       // this will always be the case
+  int debug = data->ints[1]->ints[0];
+
+  if(debug>0) {
+    fprintf(stderr, "n = %d and debug = %d\n", N, debug);
+  }
+
+  // the number of neighbors at the diagonal
+  // and the (upper) graph (-1)
+  // NOTE: scale this above def one!!!
+  assert(data->n_smats > 0);
+  assert(!strcasecmp(data->smats[0]->name, "Rgraph"));
+  inla_cgeneric_smat_tp *Rgraph = data->smats[0];
+  assert(N == Rgraph->nrow);
+  assert(N == Rgraph->ncol);
+  int M = Rgraph->n;
+  if(debug>0) {
+    fprintf(stderr, "M = %d\n", M);
+  }
+
+  switch (cmd) {
+
+  case INLA_CGENERIC_VOID:
+  {
+    assert(!(cmd == INLA_CGENERIC_VOID));
+    break;
+  }
+
+  case INLA_CGENERIC_GRAPH:
+  {
+    int offset = 2;
+    ret = Calloc(2 + 2 * M, double);
+    ret[0] = N;        	       /* dimension */
+    ret[1] = M;		   /* number of (i <= j) */
+    for (int k = 0; k < M; k++) {
+      ret[offset + k] = Rgraph->i[k];
+      ret[offset + M + k] = Rgraph->j[k];
+    }
+    break;
+  }
+
+  case INLA_CGENERIC_Q:
+  {
+    int offset = 2;
+    ret = Calloc(2 + M, double);
+    ret[0] = -1;		/* REQUIRED */
+    ret[1] = M;
+    for (int k = 0; k < M; k++) {
+      ret[offset + k] = prec * Rgraph->x[k];
+    }
+    break;
+  }
+
+  case INLA_CGENERIC_MU:
+  {
+    // return (N, mu)
+    // if N==0 then mu is not needed as its taken to be mu[]==0
+    ret = Calloc(1, double);
+    ret[0] = 0;
+    break;
+  }
+
+  case INLA_CGENERIC_INITIAL:
+  {
+    // return c(M, initials)
+    // where M is the number of hyperparameters
+    ret = Calloc(2, double);
+    ret[0] = 1;
+    ret[1] = 0.0;
+    break;
+  }
+
+  case INLA_CGENERIC_LOG_NORM_CONST:
+  {
+    break;
+  }
+
+  case INLA_CGENERIC_LOG_PRIOR:
+  {
+    // return c(LOG_PRIOR), PC-PREC
+    ret = Calloc(1, double);
+    double val = theta[0] / 2.0;
+    assert(!strcasecmp(data->doubles[0]->name, "param"));
+    double u = data->doubles[0]->doubles[0];
+    double a = data->doubles[0]->doubles[1];
+    double l = -log(a)/u;
+    if(a==0) {
+      ret[0] = 0.0;
+    } else {
+      if(a==1) {
+        ret[0] = 0.0;
+      } else {
+        ret[0] = log(l/2.0) - l * exp(-val) - val;
+      }
+    }
+    break;
+  }
+
+  case INLA_CGENERIC_QUIT:
+  default:
+    break;
+  }
+
+  return (ret);
+}
---
+++ tests/test_cgeneric_besag.R
@@ -0,0 +1,137 @@
+
+library(spdep)
+library(INLA)
+library(corGraphs)
+
+inla.setOption(
+    safe = FALSE
+)
+
+nxy <- c(100, 150)
+nb <- grid2nb(d = nxy, queen = FALSE)
+nnb <- card(nb)
+n <- length(nnb)
+
+nb.graph <- sparseMatrix(
+    i = rep(1:n, nnb),
+    j = unlist(nb[nnb>0]),
+    x = 1,
+    dims = c(n, n)
+)
+
+R0 <- inla.as.sparse(Diagonal(n, nnb) - nb.graph)
+R0[1:7, 1:7]
+
+idx <- which(R0@i >= R0@j) ## should be lower here and transposed later !!!
+ord <- order(R0@i[idx])
+Rgraph <- inla.as.sparse(
+    sparseMatrix(
+        i = R0@i[idx[ord]]+1L,
+        j = R0@j[idx[ord]]+1L,
+        x = R0@x[idx[ord]]
+    )
+)
+Rgraph@i
+Rgraph@j
+Rgraph[1:5, 1:5]
+
+m1 <- do.call(
+    "inla.cgeneric.define",
+    list(
+        model = "inla_cgeneric_besag",
+        shlib = paste0(
+            system.file(
+                "libs/",
+                package = "corGraphs"),
+            "corGraphs.so"),
+        n = n,
+        debug = TRUE,
+        Rgraph = t(Rgraph),
+        param = c(1, 0.5)
+    )
+)
+
+str(m1)
+str(m1$f$cgeneric$data$smatrices,1)
+
+theta1 <- 0
+
+str(cgeneric_get(m1, "initial"))
+str(cgeneric_get(m1, "mu", theta = theta1))
+str(cgeneric_get(m1, "log_prior", theta = theta1))
+str(g1ij <- cgeneric_get(m1, "graph"))
+str(g1ij)
+table(g1ij[[1]]<=g1ij[[2]])
+str(cgeneric_get(m1, "graph", optimize = FALSE))
+str(cgeneric_get(m1, "Q", theta = theta1))
+
+str(Q1 <- cgeneric_get(m1, "Q", theta = theta1, optimize = FALSE))
+
+image(Q1)
+
+dtest1 <- list(
+    i = 1:n,
+    x = inla.qsample(n = 1, Q = Q1 + Diagonal(n, nnb) * 0.0001,
+                     constr = list(A = matrix(1, 1, n), e = 0))[, 1]
+)
+dtest1$y <- rpois(n, exp(3 + dtest1$x))
+
+
+
+r1 <- inla(
+    y ~ f(i, model = m1, constr = FALSE) - 1,
+    data = dtest1,
+    family = 'poisson',
+    control.inla = list(int.strategy = 'eb'),
+    control.mode = list(
+        theta = theta1,
+        fixed = TRUE
+    ),
+    control.compute = list(config = TRUE)
+)
+
+ri <- inla(
+    y ~ f(i, model = 'besag', graph = nb.graph,
+          constr = FALSE,
+          scale.model = FALSE, diagonal = 0,
+          hyper = list(theta = list(prior = 'pc.prec',
+                                    param = c(1, 0.5)))) - 1,
+    data = dtest1,
+    family = 'poisson',
+    control.inla = list(int.strategy = 'eb'),
+    control.mode = list(
+        theta = theta1,
+        fixed = TRUE
+    ),
+    control.compute = list(config = TRUE)
+)
+
+rbind(r1$cpu.used, ri$cpu.used)
+
+c(r1$mode$theta, ri$mode$theta)
+
+rbind(r1$summary.fix, 
+      ri$summary.fix)
+
+diag(cor(r1$summary.random$i,
+         ri$summary.random$i))
+
+unlist(inla.zmarginal(inla.tmarginal(
+    function(x) exp(-x/2),
+    r1$internal.marginals.hyperpar[[1]]), TRUE))
+
+unlist(inla.zmarginal(inla.tmarginal(
+    function(x) exp(-x/2),
+    ri$internal.marginals.hyperpar[[1]]), TRUE))
+
+names(r1$misc$configs$config[[1]])
+
+str(r1$misc$configs$config[[1]]$Qprior)
+str(ri$misc$configs$config[[1]]$Qprior)
+
+summary(r1$misc$configs$config[[1]]$Qprior@x/
+        ri$misc$configs$config[[1]]$Qprior@x)
+
+all.equal(r1$misc$configs$config[[1]]$Qprior,
+          ri$misc$configs$config[[1]]$Qprior)
+
