fc764027598d3feaedca0cd64bd67179318a2490
Elias T Krainski <eliaskrainski@gmail.com>
2024-08-26 10:16:39 +0300

    update Classes

--- R/aaaClasses.R
+++ R/aaaClasses.R
@@ -23,7 +23,7 @@ setClass(
   "inla.rgeneric",
   slots = "f",
   validity = function(object) {
-    all(c("model", "n", "cgeneric") %in%
+    all(c("model", "n", "rgeneric") %in%
           names(object$f))
   }
 )
