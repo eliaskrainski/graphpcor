4ab1bf68c986da019c1aa01892c7b15b457206d2
Elias T Krainski <eliaskrainski@gmail.com>
2024-09-03 15:05:47 +0300

    add plot dtg

--- R/dtgUtils.R
+++ R/dtgUtils.R
@@ -1,16 +1,34 @@
-#' Directed Tree Graph - DTG definitions an dmethods
+#' Directed Tree Graph - DTG.
+#' Set as a graph with nodes representing two kind of
+#' variables: children and parent. See details.
 #' @param ... a list of formula used as relationship
 #' to define the Directed Tree Graph - DTG.
 #' Parent nodes shall be in the right side while children
 #' (or parents with a parent) in the left side.
-#' Example 1: dtg(p1 ~ c1 + c2
-#' Example 2: dtg(p1 ~ c1 + c2 + p2, p2 ~ c3)
+#' Please see the examples.
+#' @details
+#' The children variables are those with an ancestor (parent),
+#' and are identified as `c1`, ..., `cn`, where `n` is the
+#' total number of children variables.
+#' The variables are identified as `p1`, ..., `pm`,
+#' where the `m` is the number of parent variables.
+#' The main parent (fist) should be identified as `p1`.
+#' Parent variables (exept `p1`) have an ancestor,
+#' which is a parent variable.
 #' @export
 #' @examples
 #' g1 <- dtg(p1 ~ c1 + c2 - c3)
 #' g1
-#' g2 <- dtg(p1 ~ c1 + c2 + p2, p2 ~ c3 - c4)
+#' summary(g1)
+#' g2 <- dtg(p1 ~ c1 + c2 + p2,
+#'           p2 ~ c3 - c4)
 #' g2
+#' summary(g2)
+#' g3 <- dtg(p1 ~ -p2 + c1 + c2,
+#'           p2 ~ c3)
+#' g3
+#' summary(g3)
+
 dtg <- function(...) {
 
   fch <- as.character(match.call())[-1]
@@ -136,9 +154,11 @@ dtg <- function(...) {
 
 #' @export
 print.dtg <- function(x, ...) {
-  cat("DTG for", attr(x, "childrens"),
+  cat("DTG for",
+      attr(x, "childrens"),
       "childrens with",
-      attr(x, "parents"), "parents\n")
+      attr(x, "parents"),
+      "parents\n")
   for(i in 1:length(x)) {
     cat(x[[i]], "\n")
   }
@@ -149,4 +169,86 @@ summary.dtg <- function(object, ...) {
   attr(object, "relations")
 }
 
+#' @export
+setMethod(
+  "plot",
+  "dtg",
+  function(x, y, ...) {
 
+    trm <- attr(x, "relations")
+    m <- ncol(trm)
+    n <- nrow(trm)-m+1
+
+    nodes <- unique(c(colnames(trm), rownames(trm)))
+    edgl <- vector("list", m + n)
+    names(edgl) <- c(paste0("p", 1:m), paste0("c", 1:n))
+    for(i in 1:m) {
+      edgl[[i]] <- list(edges = rownames(trm)[trm[, i] != 0])
+    }
+    gr <- graph::graphNEL(
+      nodes = nodes,
+      edgeL = edgl,
+      edgemode='directed')
+
+    mc <- lapply(
+      match.call(expand.dots = TRUE)[-1],
+      eval)
+    nargs <- names(mc)
+    nattr <- lapply(
+      list(
+        color =  {
+          if(any(nargs == "color"))
+            mc$color[1:2]
+          else
+            c("red", "blue")
+          },
+        fillcolor = {
+          if(any(nargs == "fillcolor"))
+            mc$fillcolor[1:2]
+          else
+            c("lightsalmon", "lightblue")
+          },
+        shape = {
+          if(any(nargs == "shape"))
+             mc$shape[1:2]
+           else
+             c("box", "circle")
+        },
+        height = {
+          if(any(nargs == "height"))
+            mc$height[1:2]
+          else
+            c(0.5, 0.5)
+        },
+        width = {
+          if(any(nargs == "width"))
+            mc$width[1:2]
+          else
+            c(0.75, 0.75)
+        },
+        fontsize = {
+          if(any(nargs == "fontsize"))
+            mc$fontsize
+          else
+            c(14, 14)
+        }
+      ), rep, times = c(m, n))
+    for(i in 1:length(nattr))
+      names(nattr[[i]]) <- nodes
+
+    # eattr <- buildEdgeList(gr)
+    # for(i in 1:m) {
+    #   j <- which(trm[, i] != 0)
+    #   for(k in 1:length(j)) {
+    #     nam <- paste0(colnames(trm), "~", rownames(trm)[j[k]])
+    #     l <- which(names(eattr) == nam)
+    #     eattr[[l]]@attrs$weight <- paste(trm[j[k], i])
+    #   }
+    # }
+    plot(gr,
+         nodeAttrs = nattr,
+         ##edgeAttrs = eattr,
+         ...)
+
+  }
+)
