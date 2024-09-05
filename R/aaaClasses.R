0d8664d2b33a5354479a3c1c1b82969a89f7c634
Elias T Krainski <eliask@fedora.kaust.edu.sa>
2024-09-05 19:03:45 +0300

    rm extraconstr class

--- R/aaaClasses.R
+++ R/aaaClasses.R
@@ -2,15 +2,6 @@
 #'
 
 setClass(
-  "extraconstr",
-  slots = c("A", "e"),
-  validity = function(object) {
-    all(c("A", "e", "n") %in% names(object)) &&
-      (ncol(object$A) == length(object$e))
-  }
-)
-
-setClass(
   "inla.cgeneric",
   slots = "f",
   validity = function(object) {
