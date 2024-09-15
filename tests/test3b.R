3ca992d22ec870640e73670e6d96e9f50d1e3caa
eliaskrainski <eliaskrainski@gmail.com>
2024-09-15 17:56:38 +0300

    update test3b

--- tests/test3b.R
+++ tests/test3b.R
@@ -3,39 +3,38 @@ library(corGraphs)
 library(INLA)
 
 g <- dtg(
-    p1 ~ p2 + p3 + c1 + c2,
-    p2 ~ -c3 + c4,
-    p3 ~ c5 -c6)
-
+    p1 ~ p2 + c1 + c2,
+    p2 ~ p3 -c3 + c4,
+    p3 ~ c5 - c6)
 g
-
-plot(g)
 
 d <- dim(g)
 d
 
+plot(g)
+
+(np <- d[2])
 (theta.p <- c(0, -0.33, 0.33))
 
 mcov0 <- variance(g, theta = theta.p)
 mcorr <- cov2cor(mcov0)
-round(mcorr,2)
 
-nc <- d[1]
+(nc <- d[1])
 (theta.c <- seq(-1, 1, length = nc))
 
 dd <- diag(exp(theta.c))
 mcov <- dd %*% mcorr %*% dd
 
-dd^2
 round(mcov, 1)
+round(mcorr * 100)
 
-n <- 500
+n <- 300
 
 ll <- chol(mcov)
 xx <- matrix(rnorm(n * nc), n) %*% ll
 
-cov(xx)
-cor(xx)
+round(cov(xx), 1)
+round(cor(xx) * 100)
 
 dataf <- data.frame(
     i = rep(1:nc, eac = n),
@@ -50,20 +49,14 @@ gmodel <- cgeneric(
     sigma.prior.probability = rep(0.1, nc)
 )
 
-initial(gmodel)
-
 cff <- y ~ 0 + factor(i) +
     f(i, model = gmodel, replicate = r, vb.correct = FALSE)
 
 cfit <- inla(
     formula = cff,
+    ##    family = "poisson",
     control.family=list(hyper = list(prec = list(initial = 10, fixed = TRUE))),
-    data = dataf,
-    control.mode = list(
-        theta = rep(0, d[1] + d[2])
-    ), 
-    control.inla = list(int.strategy = "eb")
-    )
+    data = dataf)
 
 cfit$cpu.used
 
@@ -72,7 +65,7 @@ rbind(true = c(theta.c, theta.p),
 
 plot(cfit, F, F, F, F, F, F, plot.opt.trace = TRUE)
 
-cc.fit <- cov2cor(variance(g, theta = cfit$mode$theta[nc+1:d[2]]))
+cc.fit <- cov2cor(variance(g, theta = cfit$mode$theta[nc+1:np]))
 
 round(cor(xx)*100)
 round(cc.fit*100)
