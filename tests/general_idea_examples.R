e6096c0683395c01fa1ccfa0bba7bf2f6f78b4d1
eliaskrainski <eliaskrainski@gmail.com>
2024-09-30 13:31:37 +0300

    update genideaex

--- tests/general_idea_examples.R
+++ tests/general_idea_examples.R
@@ -33,17 +33,33 @@ plot(inla.read.graph(G3.reord))
 plot(inla.read.graph(G4))
 plot(inla.read.graph(G4.reord))
 
-thetas11 <- rep(c(log(2), -1, 1), c(6, 3, 2))
-thetas12 <- rep(c(log(2), -1, 1), c(6, 3, 3))
+thetas1 <- rep(c(log(2), -1), c(6, 5))
+thetas1b <- rep(c(log(2), 1), c(6, 5))
+thetas1c <- c(rep(log(2), 6), -1,1,-1,1,-1)
+thetas2 <- rep(c(log(2), -1), c(6, 6))
+thetas2b <- rep(c(log(2), 1), c(6, 6))
+thetas2c <- c(rep(log(2), 6), -1,1,-1,1,-1,1)
 
-library(ggpubr)
+c64 <- rgb(seq(0,1,length=64), 0.3,
+           seq(1,0,length=64))
 
-ggarrange(
-    image(cov2cor(chol2inv(t(dag_L(G1, thetas11))))),
-    image(cov2cor(chol2inv(t(dag_L(G2, thetas12))))),
-    image(cov2cor(chol2inv(t(dag_L(G3, thetas11))))),
-    image(cov2cor(chol2inv(t(dag_L(G4, thetas11)))))
-)
+par(mfrow = c(4, 4), mar = c(.5,.5,.5,.5))
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G1, thetas1))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G2, thetas2))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G3, thetas1))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G4, thetas1))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G1, thetas1b))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G2, thetas2b))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G3, thetas1b))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G4, thetas1b))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G1, thetas1c))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G2, thetas2c))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G3, thetas1c))))), col=c64, axes = FALSE)
+image(as.matrix(cov2cor(chol2inv(t(dag_L(G4, thetas1c))))), col=c64, axes = FALSE)
+plot(inla.read.graph(G1))
+plot(inla.read.graph(G2))
+plot(inla.read.graph(G3))
+plot(inla.read.graph(G4))
 
 detach("package:corGraphs", unload = TRUE)
 library("corGraphs")
