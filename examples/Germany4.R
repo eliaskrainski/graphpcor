5cdc69d70cb80f6a94eab02b7b23a145c9d4ce81
Elias T Krainski <eliaskrainski@gmail.com>
2025-03-21 10:42:07 +0300

    add graph5b in Germany4.R

--- examples/Germany4.R
+++ examples/Germany4.R
@@ -151,46 +151,61 @@ colnames(svcor.m2) <- rownames(svcor.m2) <-
 ####################################################
 
 ## graphical model for correlation
-graph5 <- graphpcor(
-    Oral_1 ~ Osph_2 + Lary_3,
-    Lung_4 ~ Osph_2 + Lary_3 + Oral_1
-)
 graph4 <- graphpcor(
-    Oral_1 ~ Osph_2 + Lary_3,
-    Lung_4 ~ Osph_2 + Lary_3
+    Oral ~ Osph + Lary,
+    Lung ~ Osph + Lary
+)
+graph5 <- graphpcor(
+    Oral ~ Osph + Lary + Lung,
+    Lung ~ Osph + Lary
+)
+graph5b <- graphpcor(
+    Oral ~ Osph + Lary,
+    Lung ~ Osph + Lary,
+    Osph ~ Lary
 )
 
-par(mfrow = c(1, 3), mar = c(0,0,0,0))
+par(mfrow = c(2, 2), mar = c(0,0,0,0))
 plot(tree3)
-plot(graph5)
 plot(graph4)
+plot(graph5)
+plot(graph5b)
 
 ### Two cgeneric graphpcor models
-g5model <- cgeneric(
-    model = graph5, 
-    lambda = 5,
-    sigma.prior.reference = rep(1, K),
-    sigma.prior.probability = rep(0.1, K)
-)
 g4model <- cgeneric(
     model = graph4, 
     lambda = 5,
     sigma.prior.reference = rep(1, K),
     sigma.prior.probability = rep(0.1, K)
 )
+g5model <- cgeneric(
+    model = graph5, 
+    lambda = 5,
+    sigma.prior.reference = rep(1, K),
+    sigma.prior.probability = rep(0.1, K)
+)
+g5bmodel <- cgeneric(
+    model = graph5b, 
+    lambda = 5,
+    sigma.prior.reference = rep(1, K),
+    sigma.prior.probability = rep(0.1, K)
+)
 
 ### The kronecker product model
-graph5Besag <- kronecker(g5model, cBesag)
 graph4Besag <- kronecker(g4model, cBesag)
+graph5Besag <- kronecker(g5model, cBesag)
+graph5bBesag <- kronecker(g5bmodel, cBesag)
 
 str(graph5Besag$f$extraconstr)
 
 m3f <- Obs ~ 0 + disease + 
-    f(iddata, model = graph5Besag)
-m4f <- Obs ~ 0 + disease + 
     f(iddata, model = graph4Besag)
+m4f <- Obs ~ 0 + disease + 
+    f(iddata, model = graph5Besag)
+m5f <- Obs ~ 0 + disease + 
+    f(iddata, model = graph5bBesag)
 
-### fit the model with graph5
+### fit the model with graph4
 res.m3 <- inla(
     formula = m3f, 
     family = "poisson",
@@ -200,9 +215,19 @@ res.m3 <- inla(
     control.inla = ctri
 )
 
-## fit the model with graph4
+## fit the model with graph5
 res.m4 <- inla(
     formula = m4f, 
+    family = "poisson",
+    data = ldata, 
+    E = E,
+    control.compute = ctrc,
+    control.inla = ctri
+)
+
+## fit the model with graph5b
+res.m5 <- inla(
+    formula = m5f, 
     family = "poisson",
     data = ldata, 
     E = E,
@@ -213,73 +238,102 @@ res.m4 <- inla(
 ## similar marginal std (see next plot)
 res.m3$summary.hyperpar[1:K, c(1,2,3,5)]
 res.m4$summary.hyperpar[1:K, c(1,2,3,5)]
+res.m5$summary.hyperpar[1:K, c(1,2,3,5)]
 
-edgl.all <- matrix(paste(rep(vnames, each = K), "~", vnames), K)
-edgl.all
+edg.all <- matrix(
+    paste(rep(vnames, each = K), "~", vnames),
+    K)
+edg.l <- edg.all[lower.tri(diag(K))]
+edg.all
+edg.l
 
-lg5 <- Laplacian(graph5)
-lg5
 lg4 <- Laplacian(graph4)
 lg4
+lg5 <- Laplacian(graph5)
+lg5
+lg5b <- Laplacian(graph5b)
+lg5b
 
-edg.g5 <- edgl.all[lower.tri(lg5) & (!is.zero(lg5))]
-edg.g4 <- edgl.all[lower.tri(lg4) & (!is.zero(lg4))]
+edg.g4 <- edg.all[lower.tri(lg4) & (!is.zero(lg4))]
+edg.g5 <- edg.all[lower.tri(lg5) & (!is.zero(lg5))]
+edg.g5b <- edg.all[lower.tri(lg5b) & (!is.zero(lg5b))]
 
-edg.g5
-res.m3$summary.hyperpar[-(1:K), c(1,2,3,5)] ## edge (Lary ~ Osph) "non-significant"
 edg.g4
-res.m4$summary.hyperpar[-(1:K), c(1,2,3,5)]
+res.m3$summary.hyperpar[-(1:K), c(1,2,3,5)]
+edg.g5
+res.m4$summary.hyperpar[-(1:K), c(1,2,3,5)] ## edge (Lary ~ Osph) "non-significant"
+edg.g5b
+res.m5$summary.hyperpar[-(1:K), c(1,2,3,5)] ## !!!
 
-par(mfrow = c(3,3), mar = c(3,3,1,0.5), mgp = c(2,0.5,0))
+ii <- list(1:4, c(1:2,4:5,3), c(1:2, 4:5,3))
+ii
+ije <- list(pmatch(edg.g4, edg.all[lower.tri(lg4)]),
+           pmatch(edg.g5, edg.all[lower.tri(lg5)]),
+           pmatch(edg.g5b, edg.all[lower.tri(lg5b)]))
+ije
+
+png("../slides/graphpcor25march/figures/marginals_std_correl.png",
+    width = 1600, height = 1200, res = 200)
+par(mfrow = c(2,5), mar = c(3,3,1,0.5), mgp = c(2,0.5,0))
 for(i in 1:K) {
     nami <- vnames[i]
     m3.i <- inla.tmarginal(exp, res.m3$internal.marginals.hyperpar[[i]])
-    m4.i <- inla.tmarginal(exp, res.m4$internal.marginals.hyperpar[[i]]) 
+    m4.i <- inla.tmarginal(exp, res.m4$internal.marginals.hyperpar[[i]])
+    m5.i <- inla.tmarginal(exp, res.m5$internal.marginals.hyperpar[[i]]) 
     plot(m3.i, type = "l", bty = 'n', lwd = 2,
-         xlim = range(m3.i[, 1], m4.i[, 1]),
-         ylim = range(m3.i[, 2], m4.i[, 2]),
+         xlim = range(m3.i[, 1], m4.i[, 1], m5.i[,1]),
+         ylim = range(m3.i[, 2], m4.i[, 2], m5.i[,2]),
          xlab = bquote(sigma[.(nami)]), ylab = 'Density')
     lines(m4.i, lwd = 3, lty = 2)
+    lines(m5.i, lwd = 4, lty = 3)
 }
-for(i in (K+1):(K+5)) {
-    edjnam <- edg.g5[i-K]
-    m3.i <- inla.smarginal(res.m3$internal.marginals.hyperpar[[i]])
-    k <- i - (i>(K+2))
-    cat(c(i=i, k=k), "\n")
-    if(i!=(K+3)) {
-        m4.i <- inla.smarginal(res.m4$internal.marginals.hyperpar[[k]])
-        xlm <- range(m3.i$x, m4.i$x)
-        ylm <- range(m3.i$y, m4.i$y)
-    } else {
-        xlm <- range(m3.i$x)
-        ylm <- range(m3.i$y)
-    }
+for(i in 1:4) {
+    edjnam <- edg.l[ije[[1]][i]]
+    m3.i <- inla.smarginal(res.m3$internal.marginals.hyperpar[[K+ii[[1]][i]]])
+    m4.i <- inla.smarginal(res.m4$internal.marginals.hyperpar[[K+ii[[2]][i]]])
+    m5.i <- inla.smarginal(res.m5$internal.marginals.hyperpar[[K+ii[[3]][i]]])
+    xlm <- range(m3.i$x, m4.i$x, m5.i$x)
+    ylm <- range(m3.i$y, m4.i$y, m5.i$y)
     plot(m3.i, type = "l", bty = 'n', lwd = 2,
          xlim = xlm, ylim = ylm, 
          xlab = edjnam, ylab = 'Density')
-    if(i!=(K+3)) {
-        lines(m4.i, lwd = 3, lty = 2)
-    }
+    lines(m4.i, lwd = 3, lty = 2)
+    lines(m5.i, lwd = 4, lty = 3)
     abline(v=0, col=gray(0.5), lwd = 2, lty = 2)
 }
+plot(inla.smarginal(res.m4$internal.marginals.hyperpar[[K+ii[[2]][5]]]),
+     type = "l", bty = "n", lwd = 3, lty = 2,
+     xlab = edg.l[ije[[2]][3]], ylab = "Density")
+abline(v=0, col=gray(0.5), lwd = 2, lty = 2)
+plot(inla.smarginal(res.m5$internal.marginals.hyperpar[[K+ii[[3]][5]]]),
+     type = "l", bty = "n", lwd = 4, lty = 3,
+     xlab = edg.l[ije[[3]][3]], ylab = "Density")
+abline(v=0, col=gray(0.5), lwd = 2, lty = 2)
+dev.off()
 
 ## covariance, correlation and std for the fitted log(smr)
 svcor.m3 <- scc.fn(
-    vcov(graph5, theta = res.m3$mode$theta),
+    vcov(graph4, theta = res.m3$mode$theta),
     mat = TRUE)
 colnames(svcor.m3) <- rownames(svcor.m3) <-
     gsub("obs", "m3", rownames(svcor.obs))
 svcor.m4 <- scc.fn(
-    vcov(graph4, theta = res.m4$mode$theta),
+    vcov(graph5, theta = res.m4$mode$theta),
     mat = TRUE)
 colnames(svcor.m4) <- rownames(svcor.m4) <-
     gsub("obs", "m4", rownames(svcor.obs))
+svcor.m5 <- scc.fn(
+    vcov(graph5b, theta = res.m5$mode$theta),
+    mat = TRUE)
+colnames(svcor.m5) <- rownames(svcor.m5) <-
+    gsub("obs", "m5", rownames(svcor.obs))
 
 round(svcor.obs, 2) ## upper.tri as in Table 2 of Held et. al. (2005)
 round(svcor.m1, 2)
 round(svcor.m2, 2) 
 round(svcor.m3, 2)
 round(svcor.m4, 2)
+round(svcor.m5, 2)
 
 if(FALSE) {
     
@@ -288,24 +342,25 @@ if(FALSE) {
         m1 = svcor.m1,
         m2 = svcor.m2,
         m3 = svcor.m3,
-        m4 = svcor.m4)
+        m4 = svcor.m4,
+        m5 = svcor.m5)
     for(i in 1:length(tbs))
         dimnames(tbs[[i]])  <- list(
             c("Oral", "Oesophagus", "Larynx", "Lung"),
             c("Oral", "Oesophagus", "Larynx", "Lung")
         )
-
     for(i in 1:length(tbs))
         tbs[[i]][lower.tri(tbs[[i]])] <- NA
 
     knitr::kable(tbs[[1]], digits = 2)
 
-    knitr::kable(tb1, digits = 2)
+    knitr::kable(tbs[[2]], digits = 2)
 
-    knitr::kable(tb2, digits = 2)
+    knitr::kable(tbs[[3]], digits = 2)
 
-    knitr::kable(tb3, digits = 2)
-    knitr::kable(tb4, digits = 2)
+    knitr::kable(tbs[[4]], digits = 2)
+    knitr::kable(tbs[[5]], digits = 2)
+    knitr::kable(tbs[[6]], digits = 2)
     
 }
 
@@ -314,7 +369,8 @@ gcpo.tab <- data.frame(
         -sum(log(r$gcpo$gcpo))), 
     apply(cbind(M2 = res.m2$gcpo$gcpo,
                 M3 = res.m3$gcpo$gcpo,
-                M4 = res.m4$gcpo$gcpo), 2,
+                M4 = res.m4$gcpo$gcpo,
+                M5 = res.m5$gcpo$gcpo), 2,
     tapply, rep(vnames, each = n.areas),
           function(x) -sum(log(x)))
 )
@@ -332,35 +388,81 @@ th.m3.samples <- t(inla.hyperpar.sample(
     n = nsampl, res.m3, intern = TRUE, improve = TRUE))
 th.m4.samples <- t(inla.hyperpar.sample(
     n = nsampl, res.m4, intern = TRUE, improve = TRUE))
+th.m5.samples <- t(inla.hyperpar.sample(
+    n = nsampl, res.m5, intern = TRUE, improve = TRUE))
 
 scc.m2 <- t(apply(th.m2.samples, 2, function(x)
-    scc.fn(vcov(tree3,theta=x), mat = TRUE)))
+    scc.fn(vcov(tree3, theta=x), mat = TRUE)))
 scc.m3 <- t(apply(th.m3.samples, 2, function(x)
-    scc.fn(vcov(graph5,theta=x), mat = TRUE)))
+    scc.fn(vcov(graph4, theta=x), mat = TRUE)))
 scc.m4 <- t(apply(th.m4.samples, 2, function(x)
-    scc.fn(vcov(graph4,theta=x), mat = TRUE)))
+    scc.fn(vcov(graph5, theta=x), mat = TRUE)))
+scc.m5 <- t(apply(th.m5.samples, 2, function(x)
+    scc.fn(vcov(graph5b, theta=x), mat = TRUE)))
 
+png("../slides/graphpcor25march/figures/posterior_correlations_histogram.png",
+    width = 1600, height = 1200, res = 200)
 par(mfrow = c(K, K), mar = c(3,2,2,0.5), mgp = c(2,0.5,0), las = 1)
 for(i in 1:K) {
     nami <- vnames[i]
     for (j in 1:K) {
-        namj <- vnames[j]
-        k <- (i-1)*K + j
-        bk <- pretty(c(scc.m2[, k], scc.m3[, k], scc.m4[, k]), 20)
-        hist(scc.m2[, k], bk, freq = FALSE, ylab = '',
-             xlab = ifelse(i==j,
-                           as.expression(bquote(sigma[.(nami)])),
-                           ifelse(i<j,
-                                  paste0("Cor(", nami, ", ", namj, ")"), 
-                                  paste0("Cov(", nami, ", ", namj, ")"))), 
-             col = gray(0.4, 0.5), border = 'transparent',
-             main = ifelse(i==j, vnames[i], ""))
-        hist(scc.m3[, k], bk, freq = FALSE, add = TRUE, 
-             col = rgb(1, 0.5, 0.3, 0.5), border = 'transparent')
-        hist(scc.m4[, k], bk, freq = FALSE, add = TRUE, 
-             col = rgb(0.3, 0.5, 1, 0.5), border = 'transparent')
+        if(j<i) {
+            if(i==4) {
+                if(j==1) {
+                    plot(graph4)
+                    legend("topleft", "graph4", bty = "n")
+                }
+                if(j==2) {
+                    plot(graph5)
+                    legend("topleft", "graph 5", bty = "n")
+                }
+                if(j==3) {
+                    plot(graph5b)
+                    legend("topleft", "graph 5b", bty = "n")
+                }
+            } else {
+                plot(0, type = "h", bty = "n", axes = FALSE, xlab = "", ylab = "")
+            }                
+        } else {
+            namj <- vnames[j]
+            k <- (j-1)*K + i
+            bk <- pretty(c(scc.m3[, k], scc.m4[, k], scc.m5[, k]), 20)
+            hist(scc.m3[, k], bk, freq = FALSE, ylab = '',
+                 xlab = ifelse(i==j,
+                               as.expression(bquote(sigma[.(nami)])),
+                        ifelse(i<j,
+                               paste0("Cor(", nami, ", ", namj, ")"), 
+                               paste0("Cov(", nami, ", ", namj, ")"))), 
+                 dens = 20, #col = gray(0.4, 0.5), border = 'transparent',
+                 main = ifelse(i==j, vnames[i], ""))
+            hist(scc.m4[, k], bk, freq = FALSE, add = TRUE, 
+                 col = rgb(1, 0.5, 0.3, 0.5), border = 'transparent')
+            hist(scc.m5[, k], bk, freq = FALSE, add = TRUE, 
+                 col = rgb(0.3, 0.5, 1, 0.5), border = 'transparent')
+        }
+        if((i==3) & (j==2)) {
+            legend("topleft", c("graph 4", "graph 5", "graph 5b"),
+                   dens = c(20,NA,NA),
+                   fill = c(1, rgb(1,0.5,0.3), rgb(0.3,0.5,1)),
+                   bty = 'n', border = 'transparent', cex = 1.5)
+        }
     }
 }
-legend("topright", c("tree", "graph 5", "graph 4"),
-       fill = c(gray(0.4), rgb(1,0.5,0.3), rgb(0.3,0.5,1)),
-       bty = 'n', border = 'transparent', cex = 2)
+dev.off()
+
+nrepl <- 3e3
+prec4 <- replicate(nrepl, prec(graph4, theta = rnorm(4))[lower.tri(diag(4))])
+prec5 <- replicate(nrepl, prec(graph5, theta = rnorm(5))[lower.tri(diag(4))])
+prec5b <- replicate(nrepl, prec(graph5b, theta = rnorm(5))[lower.tri(diag(4))])
+
+par(mfrow = c(2, 3), mar = c(4,4,1,1), mgp = c(3, 0.5, 0))
+for(i in 1:6) {
+    bk <- pretty(c(prec4[i,], prec5[i,], prec5b[i,]), n = 50)
+    hist(prec4[i,], bk, xlab = '', ylab = '', main = edg.l[i], dens = 20)
+    hist(prec5[i,], bk, add = TRUE, col = rgb(1,0.5,0.3,0.5), border = 'transparent')
+    hist(prec5b[i,], bk, add = TRUE, col = rgb(0.3,0.5,1,0.5), border = 'transparent')
+}
+legend("topleft", c("graph 4", "graph 5", "graph 5b"),
+       dens = c(20,NA,NA),
+       fill = c(1, rgb(1,0.5,0.3), rgb(0.3,0.5,1)),
+       bty = 'n', border = 'transparent', cex = 1.5)
