5f00c00a6b0bb9bd4d032c21a4f725780b8bcad9
Elias T Krainski <eliaskrainski@gmail.com>
2026-02-10 17:46:29 +0300

    upd

--- main_branch_copy_to.sh
+++ main_branch_copy_to.sh
@@ -1,10 +1,10 @@
-cp NAM* DESCR* READ*md  ../main_branch_graphpcor/
-cp R/*                  ../main_branch_graphpcor/R/
-cp data/*               ../main_branch_graphpcor/data/
-cp demo/*               ../main_branch_graphpcor/demo/
-cp examples/*           ../main_branch_graphpcor/examples/
-cp man/*                ../main_branch_graphpcor/man/
-cp vignettes/*Rmd       ../main_branch_graphpcor/vignettes/
+cp NAM* DESCR* READ*md   ../main_branch_graphpcor/
+cp R/*R                  ../main_branch_graphpcor/R/
+cp data/*                ../main_branch_graphpcor/data/
+cp demo/*                ../main_branch_graphpcor/demo/
+cp examples/*R           ../main_branch_graphpcor/examples/
+cp man/*R                ../main_branch_graphpcor/man/
+cp vignettes/*Rmd        ../main_branch_graphpcor/vignettes/
 cd ../main_branch_graphpcor/
 head -n -1 NAMESPACE > .namespacetemp; mv .namespacetemp NAMESPACE
 sed -i '/useDynLib/d' R/basepcor_utils.R
