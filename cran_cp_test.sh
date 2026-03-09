e3c7adeb1680d11fee631e5a367d1042d7988f50
Elias T Krainski <eliaskrainski@gmail.com>
2026-03-09 11:40:00 +0300

    cran test without src/

--- cran_cp_test.sh
+++ cran_cp_test.sh
@@ -1,11 +1,10 @@
 rm -rf ~/temp/graphpcor*
 mkdir ~/temp/graphpcor/
-git checkout main 
+cd ../main_branch_graphpcor/
 cp -r DESCRIPTION NAMESPACE R/ man/ demo/ ~/temp/graphpcor/
 cd vignettes/
 mkdir ~/temp/graphpcor/vignettes/
 cp preamble.tex references.bib treepcor.Rmd ~/temp/graphpcor/vignettes/
-git checkout devel
 cd ~/temp/
 R CMD build graphpcor
 R CMD check graphpcor_*.tar.gz --as-cran
