e09408ff8ab5bc852f63c6f319a1a7ea44f21310
Elias T Krainski <eliaskrainski@gmail.com>
2026-05-06 12:47:56 +0300

    namespace

--- main_branch_copy_to.sh
+++ main_branch_copy_to.sh
@@ -8,8 +8,10 @@ cp vignettes/*Rmd        ../main_branch_graphpcor/vignettes/
 cd ../main_branch_graphpcor/
 head -n -1 NAMESPACE > .namespacetemp; mv .namespacetemp NAMESPACE
 sed -i '/useDynLib/d' R/basepcor_utils.R
-git add DESCRIPTION NAMESPACE README.Rmd README.md 
-git add R/* data/* demo/* examples/* man/* vignettes/*Rmd
-git commit -m 'update main'
-git push
+
+#git add DESCRIPTION NAMESPACE README.Rmd README.md 
+#git add R/* data/* demo/* examples/* man/* vignettes/*Rmd
+#git commit -m 'update main'
+#git push
+
 cd ../graphpcor/
