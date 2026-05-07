3bb84e1ef38ed423694bbc0841d16ba4d92204d0
Elias T Krainski <eliaskrainski@gmail.com>
2026-05-07 11:15:56 +0300

    update main

--- R/aaa.R
+++ R/aaa.R
@@ -1,4 +1,3 @@
-
 #' @importFrom methods new getMethod
 #' @importFrom grDevices rgb
 #' @importFrom utils tail
@@ -7,5 +6,9 @@
 #' @importFrom numDeriv hessian
 #' @importFrom INLAtools is.zero Sparse upperPadding packageCheck
 #' @importFrom INLAtools cgeneric cgeneric_Q cgeneric_graph
-#' @importFrom INLAtools cgeneric_intial cgeneric_prior
-#'
+#' @importFrom INLAtools cgeneric_initial cgeneric_prior
+#' @importFrom graph graphNEL
+#' @importFrom Rgraphviz agopen
+#' @importFrom igraph graph_from_adjacency_matrix
+#' @useDynLib graphpcor, .registration = TRUE
+NULL
