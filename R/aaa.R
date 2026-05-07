da6b3d7a723b085768a51e4ac8a572ee802def86
Elias T Krainski <eliaskrainski@gmail.com>
2026-05-07 11:04:32 +0300

    updts

--- R/aaa.R
+++ R/aaa.R
@@ -1,4 +1,3 @@
-
 #' @importFrom methods new getMethod
 #' @importFrom grDevices rgb
 #' @importFrom utils tail
@@ -7,5 +6,6 @@
 #' @importFrom numDeriv hessian
 #' @importFrom INLAtools is.zero Sparse upperPadding packageCheck
 #' @importFrom INLAtools cgeneric cgeneric_Q cgeneric_graph
-#' @importFrom INLAtools cgeneric_intial cgeneric_prior
-#'
+#' @importFrom INLAtools cgeneric_initial cgeneric_prior
+#' @useDynLib graphpcor, .registration = TRUE
+NULL
