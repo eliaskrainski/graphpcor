283f063a3182abe13a92ca3d14863d89e6f85cfc
Elias T Krainski <eliaskrainski@gmail.com>
2026-05-07 11:30:41 +0300

    update main

--- R/aaa.R
+++ R/aaa.R
@@ -7,8 +7,6 @@
 #' @importFrom INLAtools is.zero Sparse upperPadding packageCheck
 #' @importFrom INLAtools cgeneric cgeneric_Q cgeneric_graph
 #' @importFrom INLAtools cgeneric_initial cgeneric_prior
-#' @importFrom graph graphNEL
-#' @importFrom Rgraphviz agopen
 #' @importFrom igraph graph_from_adjacency_matrix
 #' @useDynLib graphpcor, .registration = TRUE
 NULL
