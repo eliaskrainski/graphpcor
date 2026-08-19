ad931a66e89a88ac413bb88a32d8a4b78ecbd715
Elias T Krainski <eliaskrainski@gmail.com>
2026-08-19 09:16:44 +0300

    N;npars

--- inla_branch_copy_to.sh
+++ inla_branch_copy_to.sh
@@ -1,5 +1,5 @@
 cd src/
-cp  cgeneric_graphpcor.c  cgeneric.h  cgeneric_LKJ.c  cgeneric_pc_correl.c cgeneric_treepcor.c  cgeneric_Wishart.c  graphpcor.h  graphpcor_utils.c  graphpcor_utils.h ../../inla_branch_graphpcor/src/
+cp cgeneric_stds.c cgeneric_graphpcor.c  cgeneric.h  cgeneric_LKJ.c  cgeneric_pc_correl.c cgeneric_treepcor.c  cgeneric_Wishart.c  graphpcor.h  graphpcor_utils.c  graphpcor_utils.h ../../inla_branch_graphpcor/src/
 cd ../../inla_branch_graphpcor/
 git add src/* 
 git commit -m 'update inla branch'
