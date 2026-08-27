cc98dccd5b0f5658a7e315afd8a5c555372b463d
Elias T Krainski <eliaskrainski@gmail.com>
2026-08-27 17:45:53 +0300

    update main

--- R/basepcor.R
+++ R/basepcor.R
@@ -86,14 +86,9 @@ basepcor.numeric <- function(
   stopifnot(p>1)
 
   ## check iparams
-  if(is.null(iparams)) {
-    m <- length(iLtheta)
-    iparams <- 1L:m
-    attr(iparams, "m") <- m
-  } else {
-    iparams <- m_iparams_fncheck(NULL, iparams)
-    m <- attr(iparams, "m")
-  }
+  iparams <- m_iparams_fncheck(
+    length(iLtheta), iparams)
+  m <- attr(iparams, "m")
 
   if(missing(d0)) {
     d0 <- p:1
