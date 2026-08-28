468fcb1f53fed8948183013c15fb27ddebe23dbc
Elias T Krainski <eliaskrainski@gmail.com>
2026-08-28 14:09:56 +0300

    update main

--- R/Germany4.R
+++ R/Germany4.R
@@ -1,17 +1,27 @@
 #' Germany data on four disease with geometry
+#' @name Germany4
 #' @usage data(Germany4)
 #' @format sf with an 544 areas and 9 variables
 #' @description
 #' This is an `sf` object containing the observed
 #' and expected number of cases on oral, oesophagus,
 #' larynx and lung.
-#' @keywords datasets
-#' @keywords disease mapping
-#' @keywords cancer
+#' @docType data
+#' @keywords dataset
 #' @keywords Germany
+#' @keywords cancer
+#' @keywords disease
+#' @keywords mapping
+#' @keywords map
 #' @examples
 #' data(Germany4)
 #' names(Germany4)
+#' \dontrun{
 #' ## ggplot2::ggplot(Germany4) +
 #'   ## ggplot2::geom_sf(aes(fill = lung_obs/lung_exp))
+#' }
+NULL
 "Germany4"
+#' @rdname Germany4
+#' @aliases graphGermany
+"graphGermany"
