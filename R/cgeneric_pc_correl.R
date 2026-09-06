abec9a15a930507865bc096c43cd8c1a47d72930
Elias T Krainski <eliaskrainski@gmail.com>
2026-09-06 13:15:41 +0300

    allow fixed all cparams

--- R/cgeneric_pc_correl.R
+++ R/cgeneric_pc_correl.R
@@ -166,10 +166,10 @@ cgeneric.basecor <-
   }
 
   if(is.null(I0)) {
-    stop("This is not suppose to happen!")
+    warning("This is not suppose to happen!")
     I0d <- list(
       logDeterminant = 0,
-      sqrt = NULL)
+      sqrt = 0)
   } else {
     I0d <- dspd(I0)
   }
